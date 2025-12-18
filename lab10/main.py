d3f4c24090419be3251ddfcf5006abd3ae3bea0e
AzurefGit <147048918+AzurefGit@users.noreply.github.com>
2025-12-18 10:32:06 +0100

    commir

---
+++ lab10/main.py
@@ -0,0 +1,7 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import ImageFilte
+from PIL import ImageChops
+from PIL import ImageOps
+from PIL import ImageStat as stat
